e1eb1d3a57c706156dc58eaa9ad59d76093d657e
MariyaParus <cat-she@mail.ru>
2023-07-06 19:15:01 +0300

    update working_with_vacancies.py

--- src/working_with_vacancies.py
+++ src/working_with_vacancies.py
@@ -1,13 +1,13 @@
 class Vacancy:
     """Класс для работы с вакансиями"""
 
-    def __init__(self, title: str, url: str, employer: str, req: str, salary_from, salary_to):
+    def __init__(self, title: str, url: str, employer: str, req: str, salary_from=0, salary_to=0):
         self.title = title
         self.url = url
-        self.salary_to = salary_to
-        self.salary_from = salary_from
         self.employer = employer
         self.req = req
+        self.salary_from = salary_from
+        self.salary_to = salary_to
 
     def __str__(self):
         return f'{self.title}({self.url})'
@@ -16,18 +16,18 @@ class Vacancy:
         return {
             'Профессия': self.title,
             'Ссылка': self.url,
+            'Работодатель': self.employer,
+            'Требования': self.req,
             'З/п от': self.salary_from,
             'З/п до': self.salary_to,
-            'Работодатель': self.employer,
-            'Требования': self.req
 
         }
 
     def __ge__(self, other):
-        return (self.salary_from + self.salary_to) / 2 >= (other.salary_from + other.salary_to) / 2
+        return self.salary_to >= other.salary_to
 
     def __le__(self, other):
-        return (self.salary_from + self.salary_to) / 2 >= (other.salary_from + other.salary_to) / 2
+        return self.salary_to >= other.salary_to
 
     def __eq__(self, other):
-        return (self.salary_from + self.salary_to) / 2 == (other.salary_from + other.salary_to) / 2
+        return self.salary_to == other.salary_to
